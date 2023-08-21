7c44621b45eb8522365b101c6c1f6f719bd22ae1
DanTheWebDevMan <dannielzelaya@gmail.com>
2023-08-20 22:31:59 -0500

    polishing updates

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Hello. Please hire me as your next CSM, I promise to work my hardest :) </h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
