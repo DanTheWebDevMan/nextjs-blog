713d301404eda0612882f61f1b27cd51330ccb09
DanTheWebDevMan <dannielzelaya@gmail.com>
2023-08-21 22:08:15 -0500

    adding image and css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,17 @@
 import Link from 'next/link';
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
-      <h1>Hello. Please hire me as your next CSM, I promise to work my hardest :) </h1>
+    <Layout>
+     <Head>  
+        <title> First Post </title>
+        </Head> 
+      <h1>First Post </h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
